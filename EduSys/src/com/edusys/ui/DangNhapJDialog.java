d1623b82cf64199bb5574c1efe6d699c3c71a4fb
Hoang Minh Duy <minhduy12581@gmail.com>
2022-10-29 14:18:26 +0700

    update

--- EduSys/src/com/edusys/ui/DangNhapJDialog.java
+++ EduSys/src/com/edusys/ui/DangNhapJDialog.java
@@ -20,6 +20,8 @@ public class DangNhapJDialog extends javax.swing.JDialog {
 
     /**
      * Creates new form DangNhappJDialog
+     * @param parent
+     * @param modal
      */
     public DangNhapJDialog(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
@@ -207,17 +209,15 @@ public class DangNhapJDialog extends javax.swing.JDialog {
         //</editor-fold>
 
         /* Create and display the dialog */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                DangNhapJDialog dialog = new DangNhapJDialog(new javax.swing.JFrame(), true);
-                dialog.addWindowListener(new java.awt.event.WindowAdapter() {
-                    @Override
-                    public void windowClosing(java.awt.event.WindowEvent e) {
-                        System.exit(0);
-                    }
-                });
-                dialog.setVisible(true);
-            }
+        java.awt.EventQueue.invokeLater(() -> {
+            DangNhapJDialog dialog = new DangNhapJDialog(new javax.swing.JFrame(), true);
+            dialog.addWindowListener(new java.awt.event.WindowAdapter() {
+                @Override
+                public void windowClosing(java.awt.event.WindowEvent e) {
+                    System.exit(0);
+                }
+            });
+            dialog.setVisible(true);
         });
     }
 
